77ca8d97caab92dccc774bb82821f55058a8a26f
Jackson <samuel.jackson@aluno.ufca.edu.br>
2025-11-17 22:46:44 -0300

    Clarify constructor with comment on report format
    Added a comment in the constructor to clarify the report format.

---
+++ relatorio.py
@@ -0,0 +1,34 @@
+class Relatorio:
+    def __init__(self, orcamentos: list):
+        self.orcamentos = orcamentos
+      # Ainda há dificuldade em evidenciar em que formato seria agradável receber o relatório, partirei do mais básico e sem orientação por hora.
+
+    def despesas_por_categoria(self):
+        """
+        Gera um relatório de despesas por categoria.
+         """
+        pass
+
+    def despesas_por_pagamento(self):
+        """
+        Gera um relatório de despesas por forma de pagamento.
+        """
+        pass
+
+    def percentual_por_categoria(self):
+        """
+        Gera um relatório do percentual gasto por categoria.
+        """
+        pass
+
+    def mes_mais_economico(self):
+        """
+        Identifica o mês com menor gasto total.
+        """
+        pass
+
+    def comparativo_3_meses(self):
+        """
+        Gera um comparativo de gastos dos últimos 3 meses.
+        """
+        pass
