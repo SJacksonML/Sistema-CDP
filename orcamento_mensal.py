5e667c03743a2d7d65fb3019d8c97ce7c8550244
Jackson <samuel.jackson@aluno.ufca.edu.br>
2025-11-17 21:16:09 -0300

    Enhance Orcamento class documentation
    Adiciona documentação à classe Orcamento e seus métodos.

---
+++ orcamento_mensal.py
@@ -0,0 +1,13 @@
+class Orcamento:
+  """
+  Classe responsável pelo cálculo do orçamento do mês atual ou próximo em vista de ua meta de saldo final após transações de Receita e Despesa do mês anterior
+  """
+  def __init__(self, total_despesa, total_receita, saldo)
+    """
+    Utiliza como parâmetros as variáveis númericas de total de ganhos e gastos e compara com o saldo atual para estipular metas para o mês em questão.
+    """
+  def alterarLimite(self):
+    """
+    Desejado alterar o limite de gastos objetivado para o mês.
+    """
+    pass
